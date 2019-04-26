a6024ee9eedde9fd30dfccfdc97c3eddbe8262d3
Michael Walsh <mpwalsh@mit.edu>
2019-04-26 16:38:26 -0400

    Huge bug; cant use global scope, now use UserData of target

--- HelperFunctions/mouseTrack.m
+++ HelperFunctions/mouseTrack.m
@@ -9,16 +9,20 @@ function varargout = mouseTrack(target,varargin)
 %   eventdata is the typical eventdata for these actions but with the
 %   following fields appended:
 %       Positions: a vector of all positions from start to current
-%       AxisPositions: position in figure's current axis
+%       AxisPositions: position in figure's current axis or target's
+%          closest parent.
 %           - if no axis, it will be NaN(0,2)
 %       TargetObj: the target supplied to MOUSETRACK
 %   UserData begins is empty array, but can be anything; useful to persist
 %       data between callbacks. It is an optional return argument.
-%   INPUT [name] indicates name,value parameters:
+%   INPUT (argument) indicates optional positional arg [name] indicates name,value parameters:
 %       target: a graphics object the user wishes to click on
+%       (reset): Value of "reset" will remove functionality regardless of
+%          what 'n' is. Default value is 1.
 %       [start_fcn]: see above
 %       [update_fcn]: see above
 %       [stop_fcn]: see above
+%       [n]: number of times to allow interaction (default Inf)
 %   OUTPUT:
 %       figure: the parent figure used
 %       output: the output returned by stop_fcn (if supplied, mouseTrack is
@@ -28,6 +32,8 @@ function varargout = mouseTrack(target,varargin)
 %   NOTE: nargout(stop_fcn) > 0 is used to determine if output exists and
 %       call is blocking; this means it needs to be explicit; varargout will
 %       not work.
+%   NOTE: This method will use target.UserData.mouseTrack if available
+%   otherwise error.
 %
 %   EXAMPLES:
 %       >> f = figure; ax = axes;
@@ -70,98 +76,150 @@ persistent p
 if isempty(p) % Avoid having to rebuild on each function call
     p = inputParser();
     addRequired(p,'target',@isvalid);
+    addOptional(p,'reset','',@(x)any(validatestring(x,{'reset'})));
     addParameter(p,'start_fcn','',@(x)isa(x,'function_handle'));
     addParameter(p,'update_fcn','',@(x)isa(x,'function_handle'));
     addParameter(p,'stop_fcn','',@(x)isa(x,'function_handle'));
+    addParameter(p,'n',1,@(x)validateattributes(x,{'numeric'},{'positive','scalar','integer'}));
 end
 parse(p,target,varargin{:});
+if ~isempty(p.Results.reset)
+    clean_up(target);
+    return
+end
+if isstruct(target.UserData) && isfield(target.UserData,'mouseTrack')
+    error('Target already has a mouseTrack field in UserData.')
+elseif ~isstruct(target.UserData) && ~isempty(target.UserData)
+    error('Target UserData is not a struct and is not empty.')
+end
 fig = Base.getParentFigure(target);
-ax = fig.CurrentAxes;
+ax = get_axes(target);
+if isempty(ax)
+    ax = fig.CurrentAxes; % This can also be empty
+end
 
-% Can use global function scope since only one evaluation will happen at a time
-Output = [];
-UserData = [];
-Positions = NaN(0,2);
-AxisPositions = NaN(0,2);
-last_WindowButtonMotionFcn = fig.WindowButtonMotionFcn;
-last_WindowButtonUpFcn = fig.WindowButtonUpFcn;
-last_ButtonDownFcn = target.ButtonDownFcn;
+handles.args = struct(p.Results);
+handles.fig = fig;
+handles.ax = ax;
+handles.Output = [];
+handles.UserData = [];
+handles.Positions = NaN(0,2);
+handles.AxisPositions = NaN(0,2);
+handles.last_WindowButtonMotionFcn = fig.WindowButtonMotionFcn;
+handles.last_WindowButtonUpFcn = fig.WindowButtonUpFcn;
+handles.last_ButtonDownFcn = target.ButtonDownFcn;
+target.UserData.mouseTrack = handles;
 % Set callback that starts everything
 target.ButtonDownFcn = @startTrack;
 if ~isempty(p.Results.stop_fcn) && nargout(p.Results.stop_fcn) > 0
     uiwait(fig); % Blocking call if stop_fcn has output
 end
-    function startTrack(hObj,eventdata)
-        % Link button up first in case start_fcn errors
-        fig.WindowButtonUpFcn = @stopTrack;
-        warning('off','MATLAB:structOnObject')
-        Positions(end+1,:) = get(fig,'CurrentPoint');
-        if ~isempty(ax)
-             cpt = get(ax,'CurrentPoint');
-             AxisPositions(end+1,:) = cpt(1,1:2);
-        end
-        if ~isempty(p.Results.start_fcn)
-            eventdata = struct(eventdata);
-            eventdata.Positions = Positions;
-            eventdata.AxisPositions = AxisPositions;
-            eventdata.TargetObj = target;
-            if nargout(p.Results.start_fcn) > 0
-                UserData = p.Results.start_fcn(hObj,eventdata);
-            else
-                p.Results.start_fcn(hObj,eventdata);
-            end
-        end
-        % Finish linking callbacks now that we have engaged
-        fig.WindowButtonMotionFcn = @track;
-    end
-    function track(hObj,eventdata)
-        Positions(end+1,:) = get(fig,'CurrentPoint');
-        if ~isempty(ax)
-             cpt = get(ax,'CurrentPoint');
-             AxisPositions(end+1,:) = cpt(1,1:2);
-        end
-        if ~isempty(p.Results.update_fcn)
-            eventdata = struct(eventdata);
-            eventdata.Positions = Positions;
-            eventdata.AxisPositions = AxisPositions;
-            eventdata.TargetObj = target;
-            if nargout(p.Results.update_fcn) > 0
-                UserData = p.Results.update_fcn(hObj,eventdata,UserData);
-            else
-                p.Results.update_fcn(hObj,eventdata,UserData);
-            end
-        end
-    end
-    function stopTrack(hObj,eventdata)
-        fig.WindowButtonMotionFcn = last_WindowButtonMotionFcn;
-        fig.WindowButtonUpFcn = last_WindowButtonUpFcn;
-        target.ButtonDownFcn = last_ButtonDownFcn;
-        if ~isempty(p.Results.stop_fcn)
-            Positions(end+1,:) = get(fig,'CurrentPoint');
-            if ~isempty(ax)
-                cpt = get(ax,'CurrentPoint');
-                AxisPositions(end+1,:) = cpt(1,1:2);
-            end
-            eventdata = struct(eventdata);
-            eventdata.Positions = Positions;
-            eventdata.AxisPositions = AxisPositions;
-            eventdata.TargetObj = target;
-            try
-                if nargout(p.Results.stop_fcn) > 0
-                    Output = p.Results.stop_fcn(hObj,eventdata,UserData);
-                else
-                    p.Results.stop_fcn(hObj,eventdata,UserData);
-                end
-            catch err
-                warning('off','MATLAB:structOnObject')
-                uiresume(fig); % harmless to call if not waiting
-                rethrow(err)
-            end
-        end
-        warning('on','MATLAB:structOnObject')
-        uiresume(fig);
-    end
 
-varargout = {fig,Output};
+varargout = {fig,handles.Output};
 varargout = varargout(1:nargout);
+end
+
+%%% Callback functions
+function clean_up(target)
+if isstruct(target.UserData) && isfield(target.UserData,'mouseTrack')
+    handles = target.UserData.mouseTrack;
+    warning('off','MATLAB:structOnObject')
+    uiresume(handles.fig); % harmless to call if not waiting
+    handles.fig.WindowButtonMotionFcn = handles.last_WindowButtonMotionFcn;
+    handles.fig.WindowButtonUpFcn = handles.last_WindowButtonUpFcn;
+    target.ButtonDownFcn = handles.last_ButtonDownFcn;
+    target.UserData = rmfield(target.UserData,'mouseTrack');
+end
+end
+function startTrack(target,eventdata)
+handles = target.UserData.mouseTrack;
+% Link button up first in case start_fcn errors
+handles.fig.WindowButtonUpFcn = {@stopTrack,target};
+warning('off','MATLAB:structOnObject')
+handles.Positions(end+1,:) = get(handles.fig,'CurrentPoint');
+if ~isempty(handles.ax)
+    cpt = get(handles.ax,'CurrentPoint');
+    handles.AxisPositions(end+1,:) = cpt(1,1:2);
+end
+if ~isempty(handles.args.start_fcn)
+    eventdata = struct(eventdata);
+    eventdata.Positions = handles.Positions;
+    eventdata.AxisPositions = handles.AxisPositions;
+    eventdata.TargetObj = target;
+    if nargout(handles.args.start_fcn) > 0
+        handles.UserData = handles.args.start_fcn(target,eventdata);
+    else
+        handles.args.start_fcn(target,eventdata);
+    end
+end
+handles.args.n = handles.args.n - 1;
+target.UserData.mouseTrack = handles;
+% Finish linking callbacks now that we have engaged
+handles.fig.WindowButtonMotionFcn = {@track,target};
+end
+
+function track(fig,eventdata,target)
+handles = target.UserData.mouseTrack;
+handles.Positions(end+1,:) = get(fig,'CurrentPoint');
+if ~isempty(handles.ax)
+    cpt = get(handles.ax,'CurrentPoint');
+    handles.AxisPositions(end+1,:) = cpt(1,1:2);
+end
+if ~isempty(handles.args.update_fcn)
+    eventdata = struct(eventdata);
+    eventdata.Positions = handles.Positions;
+    eventdata.AxisPositions = handles.AxisPositions;
+    eventdata.TargetObj = target;
+    if nargout(handles.args.update_fcn) > 0
+        handles.UserData = handles.args.update_fcn(fig,eventdata,handles.UserData);
+    else
+        handles.args.update_fcn(fig,eventdata,handles.UserData);
+    end
+end
+target.UserData.mouseTrack = handles;
+end
+
+function stopTrack(fig,eventdata,target)
+handles = target.UserData.mouseTrack;
+if ~isempty(handles.args.stop_fcn)
+    handles.Positions(end+1,:) = get(fig,'CurrentPoint');
+    if ~isempty(handles.ax)
+        cpt = get(handles.ax,'CurrentPoint');
+        handles.AxisPositions(end+1,:) = cpt(1,1:2);
+    end
+    eventdata = struct(eventdata);
+    eventdata.Positions = handles.Positions;
+    eventdata.AxisPositions = handles.AxisPositions;
+    eventdata.TargetObj = target;
+    try
+        if nargout(handles.args.stop_fcn) > 0
+            handles.Output = handles.args.stop_fcn(target,eventdata,handles.UserData);
+        else
+            handles.args.stop_fcn(fig,eventdata,handles.UserData);
+        end
+    catch err
+        if handles.args.n == 0
+            clean_up(target);
+        else % Suspend button motion fcn until click again
+            handles.fig.WindowButtonMotionFcn = [];
+        end
+        rethrow(err)
+    end
+end
+if handles.args.n == 0
+    clean_up(target);
+else % Suspend button motion fcn until click again
+    handles.fig.WindowButtonMotionFcn = [];
+end
+end
+
+%%% Helper functions
+function obj = get_axes(obj)
+% Given a descendant of an axes, return the axes
+while ~isempty(obj) && ~strcmp('axes', get(obj,'type'))
+  obj = get(obj,'parent');
+end
+if ~strcmp('axes', get(obj,'type'))
+    obj = [];
+end
 end
