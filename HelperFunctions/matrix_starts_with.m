2f13ab7a7a43017c7bd2b43511bf72b0cfc239a1
Michael Walsh <mpwalsh@mit.edu>
2019-07-12 17:21:50 -0400

    Fixed issue with cfit

--- HelperFunctions/matrix_starts_with.m
+++ HelperFunctions/matrix_starts_with.m
@@ -13,6 +13,13 @@ szb = size(b);
 assert(length(sza)==length(szb),'Matrix shapes must agree.')
 
 starts_with = false; % Assume false
+if isa(a,'cfit') && isa(b,'cfit') % cfit is special since indexing evaluates fit at that value
+    starts_with = isequal(a,b);
+    return 
+elseif isa(a,'cfit')
+   % Special case where cfit.subsref will fail
+   return % If a is a cfit and b is not, obviously not equal
+end
 if any(szb>sza)
     % Trivial case of b being larger than a (e.g. b=[1,2,3,4] cant be a
     % subset of a=[1,2])
@@ -23,6 +30,11 @@ S = struct('type','()','subs',{cell(0)});
 for i = 1:length(szb)
     S.subs(end+1) = {1:szb(i)};
 end
-starts_with = isequal(subsref(a,S),b);
+try
+    sub_a = subsref(a,S);
+catch err
+    error('Failed to evaluate subsref for type %s: \n%s.',class(a),err.message);
+end
+starts_with = isequal(sub_a,b);
 end
 
