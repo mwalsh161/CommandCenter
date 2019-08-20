d23ba1645a7738d590ec294c2e5d3fe8fb50d4c0
Michael Walsh <mpwalsh@mit.edu>
2019-08-20 13:37:03 -0400

    Better display

--- Modules/+Sources/@VelocityLaser/TuneCoarse.m
+++ Modules/+Sources/@VelocityLaser/TuneCoarse.m
@@ -12,15 +12,17 @@ in_bound_flag = true;
 obj.locked = false; %whether errored or not, should no longer be locked
 obj.tuning = true;
 if obj.debug
-    f_debug = UseFigure(class(obj,'class_name'),'name','TuneCoarse',true);
+    f_debug = UseFigure(mfilename('class'),'name','TuneCoarse',true);
     figure(f_debug); % Bring to front (and gcf)
-    ax_debug = axes('parent',f);
+    ax_debug = axes('parent',f_debug);
     hold(ax_debug,'on');
-    p_debug(1) = plot(ax_debug,[0 1], [target target],'--k');
+    p_debug(1) = plot(ax_debug,[0 1], [target target],'k');
+    p_debug(5) = plot(ax_debug,[0 1], [target target]-FineThresh,'--k');
+    p_debug(6) = plot(ax_debug,[0 1], [target target]+FineThresh,'--k');
     p_debug(2) = plot(ax_debug,NaN, NaN,'.-');
     p_debug(3) = plot(ax_debug,NaN, NaN,'.-');
     p_debug(4) = plot(ax_debug,NaN, NaN,'k*');
-    legend(p_debug,{'Target','SetPoint','Last Measured','Out of Range'});
+    legend(p_debug(1:4),{'Target','SetPoint','Last Measured','Out of Range'});
     ylabel(ax_debug,'Frequency (THz)');
     xlabel(ax_debug,'Iteration')
 end
@@ -36,10 +38,11 @@ try
         if obj.debug
             p_debug(2).YData = [p_debug(2).YData freq(1)];
             p_debug(3).YData = [p_debug(3).YData freq(2)];
-            x_debug = 1:length(p_debug(2).YData);
-            p_debug(1).XData = x_debug([1 end]);
-            p_debug(2).XData = x_debug;
-            p_debug(3).XData = x_debug;
+            x_debug = 0:length(p_debug(2).YData)-1; % Ignore initial NaN
+            set(p_debug([1,5,6]),'xdata',x_debug([1 end])+0.5);
+            set(p_debug(2),'xdata',x_debug);
+            set(p_debug(3),'xdata',x_debug-0.5);
+            drawnow;
         end
         try
             obj.setMotorFrequency(freq(1));
@@ -47,7 +50,7 @@ try
         catch sub_err
             if contains(sub_err.message,'Out of Range') && in_bound_flag
                 if obj.debug
-                    plot(ax_debug,x_debug(end), freq(1),'k*');
+                    plot(ax_debug,x_debug(end), freq(1),'k*','handlevisibility','off');
                 end
                 temp = mean(freq);
                 if temp > freq(1) && temp >= target % Near the low end of range
@@ -63,6 +66,11 @@ try
         freq(2) = obj.getFrequency;
         % "Logical beginning" of PID algorithm loop
         freq(1) = freq(1) + Pgain*(target - freq(2)); %take difference, use to set again
+    end
+    if obj.debug % Grab that last observation
+        p_debug(3).YData = [p_debug(3).YData freq(2)];
+        x_debug = 0:length(p_debug(3).YData)-1; % Ignore initial NaN
+        set(p_debug(3),'xdata',x_debug-0.5);
     end
     obj.setpoint = target;
     obj.tuning = false;
