c65cbfd5b947ef6aae73c9d8910b0a5fb85280b9
Michael Walsh <mwalsh161@gmail.com>
2019-09-02 22:09:53 -0400

    Consolidated code

--- +Prefs/Integer.m
+++ +Prefs/Integer.m
@@ -1,11 +1,8 @@
-classdef Integer < Base.pref
+classdef Integer < Prefs.Double
     %INTEGER Allows any integer within max and min limits
     
     properties
-        ui = Prefs.Inputs.CharField;
-        allow_nan = true;
-        max = Inf;
-        min = -Inf;
+        % See Prefs.Double
     end
     
     methods
@@ -13,26 +10,10 @@ classdef Integer < Base.pref
             obj.default = 0;
             obj = obj.init(varargin{:});
         end
-        function val = get_ui_value(obj)
-            valstr = obj.ui.get_value();
-            if strcmpi(valstr,'nan')
-                val = NaN;
-                return
-            end
-            val = str2double(valstr);
-            if isnan(val)
-                error('SETTINGS:bad_ui_val','Cannot convert "%s" to numeric value.',valstr)
-            end
-        end
         function validate(obj,val)
-            validateattributes(val,{'numeric'},{'integer','scalar'})
-            if ~obj.allow_nan
-                assert(~isnan(val),'Attempted to set NaN. allow_nan is set to false.')
-            elseif isnan(val)
-                return
-            end
-            assert(val <= obj.max, 'Cannot set value greater than max.')
-            assert(val >= obj.min, 'Cannot set value less than min.')
+            validate@Prefs.Double(obj,val);
+            % Only further check is integer
+            validateattributes(val,{'numeric'},{'integer'})
         end
     end
     
