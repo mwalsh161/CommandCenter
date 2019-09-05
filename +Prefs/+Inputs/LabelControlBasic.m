09132b87986afdbdc4efb55edd941e300ba287b1
Michael Walsh <mpwalsh@mit.edu>
2019-09-04 20:18:22 -0400

    Renamed to ModuleInstance; skeleton of ModuleSelectionField

---
+++ +Prefs/+Inputs/LabelControlBasic.m
@@ -0,0 +1,74 @@
+classdef LabelControlBasic < Base.input
+    % LABELCONTROLBASIC provides functionality for preparing a common input type
+    %   of a right justified label (text) and a left justified input uicontrol element
+    %   It stores the label uicontrol element in "label" and the input element
+    %   in "ui"
+    %   In its most basic form, this defaults to a CharField
+
+    properties % Handles to all uicontrol objects
+        label = gobjects(1)
+        ui = gobjects(1)
+    end
+    properties(Abstract,Hidden)
+        uistyle;   % uicontrol style argument
+    end
+
+    methods(Abstract)
+        % Provide function to get the label (without the ": " part)
+        % Important to do it this way to allow MATLAB to make the proper extent
+        %   when generating the label uicontrol for "label_width_px"
+        labeltext = get_label(obj,pref);
+    end
+
+    methods % Satisfy all abstract methods
+        function tf = isvalid(obj)
+            tf = isgraphics(obj.ui) && isvalid(obj.ui);
+        end
+        % These methods are responsible for building the settings UI and setting/getting values from it
+        function [obj,height_px,label_width_px] = make_UI(obj,pref,parent,yloc_px,width_px)
+            % Here, widths will all be taken care of in adjust_UI
+            tag = strrep(pref.name,' ','_');
+            labeltext = obj.get_label(pref);
+            enabled = 'on';
+            if pref.readonly
+                enabled = 'off';
+            end
+            obj.label = uicontrol(parent, 'style', 'text',...
+                        'string', [labeltext ': '],...
+                        'horizontalalignment', 'right',...
+                        'units', 'pixels',...
+                        'tag', [tag '_label']);
+            obj.label.Position(2) = yloc_px;
+            label_width_px = obj.label.Extent(3);
+
+            obj.ui = uicontrol(parent, 'style', obj.uistyle,...
+                        'horizontalalignment','left',...
+                        'units', 'pixels',...
+                        'tag', tag,...
+                        'enable', enabled);
+            obj.ui.Position(2) = yloc_px;
+
+            if ~isempty(pref.help_text)
+                set([obj.label, obj.ui], 'ToolTip', pref.help_text);
+            end
+            height_px = obj.ui.Position(4);
+        end
+        function link_callback(obj,callback)
+            obj.ui.Callback = callback;
+        end
+        function adjust_UI(obj,suggested_label_width_px)
+            pad = obj.label.Position(1); % Use pad from left for the right as well
+            obj.label.Position(3) = suggested_label_width_px;
+            obj.ui.Position(1) = suggested_label_width_px + pad;
+            obj.ui.Position(3) = obj.label.Parent.Position(3) - ...
+                                (suggested_label_width_px + 2*pad);
+        end
+        function set_value(obj,val)
+            obj.ui.String = val;
+        end
+        function val = get_value(obj)
+            val = obj.ui.String;
+        end
+    end
+
+end
---
+++ +Prefs/+Inputs/ModuleSelectionField.m
@@ -0,0 +1,63 @@
+classdef ModuleSelectionField < Base.input
+    %MODULESELECTIONFIELD provides UI to choose modules and access their settings
+
+    properties
+    end
+
+    methods % Satisfy all abstract methods
+        function tf = isvalid(obj)
+            
+        end
+        % These methods are responsible for building the settings UI and setting/getting values from it
+        function [obj,height_px,label_width_px] = make_UI(obj,pref,parent,yloc_px,width_px)
+            % Here, widths will all be taken care of in adjust_UI
+            tag = strrep(pref.name,' ','_');
+            if ~isempty(pref.units)
+                nameFormatted = sprintf('%s (%s): ',pref.name,pref.units);
+            else
+                nameFormatted = sprintf('%s: ',pref.name);
+            end
+            enabled = 'on';
+            if pref.readonly
+                enabled = 'off';
+            end
+            obj.label = uicontrol(parent, 'style', 'text',...
+                        'string', nameFormatted,...
+                        'horizontalalignment', 'right',...
+                        'units', 'pixels',...
+                        'tag', [tag '_label']);
+            obj.label.Position(2) = yloc_px;
+            label_width_px = obj.label.Extent(3);
+
+            obj.ui = uicontrol(parent, 'style', 'popupmenu',...
+                        'String',pref.choices_strings,...
+                        'horizontalalignment','left',...
+                        'units', 'pixels',...
+                        'tag', tag,...
+                        'enable', enabled);
+            obj.ui.Position(2) = yloc_px;
+
+            if ~isempty(pref.help_text)
+                set([obj.label, obj.ui], 'ToolTip', pref.help_text);
+            end
+            height_px = obj.ui.Position(4);
+        end
+        function link_callback(obj,callback)
+            obj.ui.Callback = callback;
+        end
+        function adjust_UI(obj,suggested_label_width_px)
+            pad = obj.label.Position(1); % Use pad from left for the right as well
+            obj.label.Position(3) = suggested_label_width_px;
+            obj.ui.Position(1) = suggested_label_width_px + pad;
+            obj.ui.Position(3) = obj.label.Parent.Position(3) - ...
+                                (suggested_label_width_px + 2*pad);
+        end
+        function set_value(obj,val)
+            
+        end
+        function val = get_value(obj)
+            
+        end
+    end
+
+end
