0d614be80f560b8bd8afc21f66f410c6003626a5
ldesanti <ldesanti@mit.edu>
2019-09-18 16:15:27 -0400

    Base experiment for super res PL scan with CW laser

---
+++ Modules/+Experiments/+SuperResScan/@SuperResScanCW/SuperResScanCW.m
@@ -0,0 +1,102 @@
+classdef SuperResScanCW < Modules.Experiment
+    
+    properties
+        abort_request = false;
+        prefs = {'resLaser','repumpLaser','scan_type','wavelengths_set','resonator_percents','useROI','xmin','xmax','xpoints','ymin','ymax','ypoints'};
+        show_prefs = {'resLaser','repumpLaser','scan_type','wavelengths_set','resonator_percents','useROI','xmin','xmax','xpoints','ymin','ymax','ypoints'};
+        wavelengths = linspace(0,100,101);
+        percents = linspace(0,100,101);
+    end
+    properties(SetObservable)
+        resLaser = Modules.Source.empty;
+        repumpLaser = Modules.Source.empty;
+        scan_type = {'Wavelength','Resonator'};
+        wavelengths_set = 'linspace(615,625,101)';
+        resonator_percents = 'linspace(0,100,101)';
+        useROI = true;
+        xmin = -1;
+        xmax = 1;
+        xpoints = 100;
+        ymin = -1;
+        ymax = 1;
+        ypoints = 100;
+    end
+    properties(Constant)
+        c = 299792; %speed of light for converting wavelength in nm to frequency in THz
+    end
+    properties (SetAccess=private)
+        data = [];
+        meta = [];
+        pb;
+        nidaq;
+        galvos;
+    end
+    
+    methods(Access=private)
+        function obj = SuperResScanCW()
+            obj.loadPrefs;
+            obj.galvos = Drivers.NIDAQ.stage.instance('X','Y','Z','APD1','GalvoScanSync');
+            obj.nidaq = Drivers.NIDAQ.dev.instance('Dev1');
+            %obj.pb = Drivers.PulseBlaster.Remote.instance(obj.ip);
+        end
+    end
+    
+    methods(Static)
+        function obj = instance()
+            mlock;
+            persistent Object
+            if isempty(Object) || ~isvalid(Object)
+                Object = Experiments.SuperResScan.SuperResScanCW();
+            end
+            obj = Object;
+        end
+    end
+    
+    methods
+        run(obj,statusH,managers,ax)
+        function abort(obj)
+            % Callback for when user presses abort in CC
+            obj.abort_request = true;
+        end
+        function dat = GetData(obj,~,~)
+            % Callback for saving methods (note, lots more info in the two managers input!)
+            dat.data = obj.data;
+            dat.meta = obj.meta;
+        end
+        
+        function set.wavelengths_set(obj,val)
+            tempvals = eval(val);
+            obj.wavelengths = tempvals;
+            obj.wavelengths_set = val;
+        end
+        function set.resonator_percents(obj,val)
+            tempvals = eval(val);
+            obj.percents = tempvals;
+            obj.resonator_percents = val;
+        end
+        function set.xmin(obj,val)
+            assert(isnumeric(val),'Value must be a number');
+            obj.xmin = val;
+        end
+        function set.xmax(obj,val)
+            assert(isnumeric(val),'Value must be a number');
+            obj.xmax = val;
+        end
+        function set.xpoints(obj,val)
+            assert(isnumeric(val),'Value must be a number');
+            obj.xpoints = val;
+        end
+        function set.ymin(obj,val)
+            assert(isnumeric(val),'Value must be a number');
+            obj.ymin = val;
+        end
+        function set.ymax(obj,val)
+            assert(isnumeric(val),'Value must be a number');
+            obj.ymax = val;
+        end
+        function set.ypoints(obj,val)
+            assert(isnumeric(val),'Value must be a number');
+            obj.ypoints = val;
+        end
+    end
+end
---
+++ Modules/+Experiments/+SuperResScan/@SuperResScanCW/run.m
@@ -0,0 +1,60 @@
+function run(obj,statusH,managers,ax)
+    
+    obj.abort_request = false;
+    status.String = 'Experiment started';
+    drawnow;
+
+    if obj.useROI
+        obj.xmin = managers.Imaging.ROI(1,1);
+        obj.xmax = managers.Imaging.ROI(1,2);
+        obj.ymin = managers.Imaging.ROI(2,1);
+        obj.ymax = managers.Imaging.ROI(2,2);
+        obj.xpoints = managers.Imaging.active_module.resolution(1);
+        obj.ypoints = managers.Imaging.active_module.resolution(2);
+    else
+        managers.Imaging.setROI([obj.xmin obj.xmax; obj.ymin obj.ymax]);
+        managers.Imaging.active_module.resolution = [obj.xpoints obj.ypoints];
+    end
+
+    if strcmp(obj.scan_type,'Wavelength')
+        values = obj.wavelengths;
+    else
+        values = obj.percents;
+    end
+    
+    try
+        for i=1:length(values)
+            obj.resLaser.off;
+            if strcmp(obj.scan_type,'Wavelength')
+                try
+                    obj.resLaser.TuneSetpoint(299792/values(i));
+                catch
+                    pause(60); % in case Msquared is doing the pump beam alignment
+                    obj.resLaser.TuneSetpoint(299792/values(i));
+                end
+            else
+                obj.resLaser.TunePercent(values(i));
+            end
+
+            resLaserWL = obj.resLaser.getWavelength;
+            obj.resLaser.on;
+            managers.Imaging.snap(true);
+            if ~managers.Imaging.last_sandboxed_fn_eval_success
+                error('Imaging failed');
+            end
+
+            obj.data.scans(i).image = managers.Imaging.current_image.info;
+            obj.data.scans(i).Setpoint = values(i);
+            obj.data.scans(i).resLaser = resLaserWL;
+            obj.resLaser.off;
+            
+            assert(~obj.abort_request,'User aborted');
+        end
+    catch run_err
+        if ~isempty(run_err)
+            obj.resLaser.off;
+            rethrow(run_err)
+        end
+    end
+
+end
