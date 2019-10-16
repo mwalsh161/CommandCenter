b1c85034982c11fb03552dd30f5d7d39e2a66106
Matthew Feldman <matthew.feldman@vanderbilt.edu>
2019-10-16 00:47:46 -0400

    removed superfluous callbacks for MWSwitch_PS

--- Modules/+Sources/MWswitch_PS.m
+++ Modules/+Sources/MWswitch_PS.m
@@ -3,14 +3,19 @@ classdef MWswitch_PS < Modules.Source
     %   Detailed explanation goes here
     % This is to turn the MW switch on and off using Pulse Streamer channels.
     
-    properties
+   
+    properties(SetObservable)
         PSline = 4;                  % Pulse Blaster flag bit (indexed from 1)
-        ip = 'No Server';         % ip of host computer (with PB)
-        prefs = {'PSline','ip'};
+        ip = 'No Server';         % ip of host computer (with PS)
+        enabled = false;
     end
     properties(SetObservable,SetAccess=private)
         source_on = false;
-        running                      % Boolean specifying if StaticLines program running
+        running = false;                    % Boolean specifying if StaticLines program running
+    end
+    properties
+          prefs;
+          show_prefs;
     end
     properties(Access=private)
         listeners
@@ -22,11 +27,9 @@ classdef MWswitch_PS < Modules.Source
     
     methods(Access=protected)
         function obj = MWswitch_PS()
-       %     obj.PulseStreamerHandle = Drivers.PulseStreamer.StaticLines.instance(obj.ip);  This is accomplished in loadPrefs (set.ip)
+            obj.prefs = [{'PSline','ip'} obj.prefs];
+            obj.show_prefs = [{'running','PSline','ip'} obj.show_prefs];
             obj.loadPrefs;
-            obj.PulseStreamerHandle.off();
-            obj.listeners = addlistener(obj.PulseStreamerHandle,'running','PostSet',@obj.isRunning);
-            
         end
     end
     methods(Static)
@@ -34,7 +37,7 @@ classdef MWswitch_PS < Modules.Source
             mlock;
             persistent Object
             if isempty(Object) || ~isvalid(Object)
-                Object = Sources.MWswitch_PB();
+                Object = Sources.MWswitch_PS();
             end
             obj = Object;
         end
@@ -42,6 +45,7 @@ classdef MWswitch_PS < Modules.Source
     methods
         function delete(obj)
             delete(obj.listeners)
+            obj.PulseStreamerHandle.delete();
         end
         function set.ip(obj,val)
             if strcmp('No Server',val)
@@ -54,7 +58,7 @@ classdef MWswitch_PS < Modules.Source
             err = [];
             try
                 obj.PulseStreamerHandle = Drivers.PulseStreamerMaster.PulseStreamerMaster.instance(val); %#ok<*MCSUP>
-                obj.PulseStreamerHandle.off();
+                obj.off();
                 delete(obj.listeners)
                 obj.listeners = addlistener(obj.PulseStreamerHandle,'running','PostSet',@obj.isRunning);
                 obj.ip = val;
@@ -81,36 +85,15 @@ classdef MWswitch_PS < Modules.Source
             state = PulseStreamer.OutputState([],0,0);
             obj.PulseStreamerHandle.PS.constant(state);
         end
+        function arm(obj)
+            obj.enabled = true;
+        end
+        function blackout(obj)
+            obj.off()
+            %possibly add code to depower switch (assuming it can be
+            %powered by nidaq)
+        end
         
-        % Settings and Callbacks
-        function  settings(obj,panelH,~,~)
-            spacing = 1.5;
-            num_lines = 3;
-            line = 1;
-            obj.status = uicontrol(panelH,'style','text','string','Unknown State, to update, change state.','horizontalalignment','center',...
-                'units','characters','position',[0 spacing*(num_lines-line) 46 1.25]);
-            obj.isRunning;
-            line = 2;
-            uicontrol(panelH,'style','text','string','PulseStreamer Line (indexed from 1):','horizontalalignment','right',...
-                'units','characters','position',[0 spacing*(num_lines-line) 35 1.25]);
-            uicontrol(panelH,'style','edit','string',num2str(obj.PSline),...
-                'units','characters','callback',@obj.PSlineCallback,...
-                'horizontalalignment','left','position',[36 spacing*(num_lines-line) 10 1.5]);
-            line = 3;
-            uicontrol(panelH,'style','text','string','PulseStreamer Host IP:','horizontalalignment','right',...
-                'units','characters','position',[0 spacing*(num_lines-line) 25 1.25]);
-            uicontrol(panelH,'style','edit','string',obj.ip,...
-                'units','characters','callback',@obj.ipCallback,...
-                'horizontalalignment','left','position',[26 spacing*(num_lines-line) 20 1.5]);
-        end
-        function PSlineCallback(obj,src,varargin)
-            val = str2double(get(src,'string'));
-            assert(round(val)==val&&val>=0,'Line number must be an integer greater than or equal to 0 for Pulse Streamer.')
-            obj.PSline = val;
-        end
-        function ipCallback(obj,src,varargin)
-            obj.ip = get(src,'string');
-        end
         function isRunning(obj,varargin)
             obj.running = obj.source_on;
             if ~isempty(obj.status)&&isvalid(obj.status)
