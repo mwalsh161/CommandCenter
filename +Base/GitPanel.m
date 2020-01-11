29c2d1c3bb0ff532cd685c1f9221fb6c4cc530bc
Ian Christen <ichr@mit.edu>
2020-01-11 14:55:05 -0500

    More git integration and UI improvements.  + Made UIScrollPanelContainer able to use non-UIScrollPanel uipanels.  + Integrated into CommandCenter GUI.  + Made loading figure sexier.

--- +Base/GitPanel.m
+++ +Base/GitPanel.m
@@ -3,43 +3,74 @@ classdef GitPanel
     properties
         panel = [];
         text = [];
-        dots = [];
         menu = [];
+    end
+    
+    properties (Constant)
+        enableLeftClick = false
     end
     
     methods
         function obj = GitPanel(varargin)
-            f = figure;
-            f.Position(3:4) = f.Position(3:4)/1.8;
+            if numel(varargin) == 2
+                assert(isprop(varargin{1}, 'Type') && strcmp(varargin{1}.Type, 'uipanel'), 'Base.GitPanel(panel, figure) expects a uipanel.')
+                assert(isprop(varargin{2}, 'Type') && strcmp(varargin{2}.Type, 'figure'), 'Base.GitPanel(panel, figure) expects a figure.')
+                obj.panel = varargin{1};
+                f = varargin{2};
+            else    % Ignore arguments otherwise
+                f = figure;
+                f.Position(3:4) = f.Position(3:4)/2;
             
-            obj.panel = uipanel(f, 'Title', 'git branch', 'Units', 'characters');
+                obj.panel = uipanel(f);
+            end
+            
+            obj.panel.Title = 'git branch';
+            obj.panel.Units = 'characters';
             obj.panel.Position(4) = 2;
             
-            obj.menu = uicontextmenu();
-            uimenu(obj.menu, 'Label', '<html><font color="purple">git</font> fetch</html>', 'Callback', @(s,e)obj.update)
-            uimenu(obj.menu, 'Label', '<html><font color="purple">git</font> add .<br><font color="purple">git</font> commit<br><html><font color="purple">git</font> push</html>')
-%             uimenu(obj.menu, 'Label', '<html><font color="purple">git</font> push')
-            uimenu(obj.menu, 'Label', '<html><font color="purple">git</font> pull')
-            obj.text = uicontrol(obj.panel, 'Style', 'radiobutton', 'UIContextMenu', obj.menu, 'Units', 'characters', 'Callback', @(s,e)obj.update);
-            obj.text.Position(1:2) = [-2.75 0];
-            obj.text.Position(3) = 10000;
+%             '<sup>--author **you**</sup> <sub>--message **decription**</sub>'
+            filedetails = ' &lt;file(s)&gt; (--all)';
+            commitdetails = ' --author &lt;you&gt; --message &lt;description&gt;';
             
-%             obj.dots = uicontrol(obj.panel, 'Style', 'text', 'String', ' ...     ', 'UIContextMenu', obj.menu, 'Units', 'characters');
-            obj.dots.Position(1:2) = [obj.panel.Position(3)-2.5 0];
-            obj.dots.Position(3) = 3;
+            obj.menu = uicontextmenu(f);
+            uimenu(obj.menu, 'Label',  '<html><font color="purple">git</font> fetch</html>', 'Callback', @(s,e)obj.update)
+            uimenu(obj.menu, 'Label', ['<html><font color="purple">git</font> status<br>'...
+                                             '<font color="purple">git</font> ad&d' filedetails '<br>'...
+                                             '<font color="purple">git</font> commit' commitdetails '<br>'...
+                                             '<font color="purple">git</font> push</html>'], 'Callback', @(s,e)(disp('Add files to a commit, then commit, and push to save changes.')));
+            uimenu(obj.menu, 'Label', ['<html><font color="purple">git</font> status<br>',...
+                                             '<font color="purple">git</font> pull</html>'], 'Callback', @(s,e)(disp('Pull to stay up to date')))
+            
+            obj.panel.UIContextMenu = obj.menu;
+            
+            obj.text = uicontrol(obj.panel, 'Style', 'radiobutton', 'UIContextMenu', obj.menu, 'Units', 'characters');
+            obj.text.Position(1:2) = [-2.75 0];
+            obj.text.Position(3) = 200;
+            
+            if (obj.enableLeftClick)
+                obj.panel.ButtonDownFcn = @(s,e)(obj.update);
+                obj.text.Callback = @(s,e)obj.update;
+            end
             
             obj.update();
         end
         
         function update(obj)
+            obj.panel.HighlightColor = [.5 0 .5];   % Purple
+            drawnow;
             try
                 git('fetch -q --all');
-            catch
-                
-            end
             
-            obj.text.String = obj.info();
-            obj.text.Tooltip = obj.tooltip();
+                obj.text.String = obj.info();
+                obj.text.Tooltip = obj.tooltip();
+                obj.panel.HighlightColor = 'w';
+                drawnow;
+            catch err
+                obj.text.String = '<html>Could not <font color="purple">git</font> fetch.';
+                obj.text.Tooltip = 'Something terrible happened.';
+                
+                rethrow(err);
+            end
         end
     end
     methods (Static)
@@ -54,7 +85,7 @@ classdef GitPanel
 %             thisbranch = 'measurement aa2a21d [ahead 6] Finished rename of files. More commenting and cleanup.';
         end
         function str = info()
-            thisbranch = Base.GitPanel.thisbranch();
+            thisbranch = makeHTML(Base.GitPanel.thisbranch());
             
             words_ = split(thisbranch, ' ');
             words = {};
@@ -62,16 +93,9 @@ classdef GitPanel
             jj = 1;
             for ii = 1:length(words_)
                 if ~isempty(words_{ii})
-                    words{jj} = words_{ii};
+                    words{jj} = words_{ii}; %#ok<AGROW>
                     jj = jj + 1;
                 end
-            end
-
-            third = words{3};
-            
-            test = '';
-            if numel(third) >= 6
-                test = third(1:6);
             end
             
             message_ = split(thisbranch, words{2});
@@ -84,6 +108,13 @@ classdef GitPanel
             end
             
             message = '';
+
+            third = words{3};
+            
+            test = '';
+            if numel(third) >= 6
+                test = third(1:6);
+            end
 
             switch test
                 case '[ahead'
@@ -113,15 +144,18 @@ classdef GitPanel
             str = {['<html><font color="blue"><B>' words{1} '</B>&nbsp;&nbsp;<I>' words{2} '</I></font>' message '</html>'], 'Untracked'};
         end
         function str = tooltip()
-            str_ = strrep(git('status --ahead-behind'), '/', ' / ');
+            str_ = strrep(git('status --ahead-behind --show-stash'), '/', ' / ');
             str__ = split(str_, newline);
             
             assert(~isempty(str__));
             
             str = ['On branch ' Base.GitPanel.thisbranch() newline str_((numel(str__{1})+1):end)];
-%             str
-%             str = ['<html><font face="Courier">' git('status') '</font></html>'];
         end
     
     end
+end
+
+function str = makeHTML(str)    % Not rigorous
+    str = strrep(str, '>', '&gt;');
+    str = strrep(str, '<', '&lt;');
 end
