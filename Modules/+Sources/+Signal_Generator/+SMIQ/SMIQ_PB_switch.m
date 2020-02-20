ea3d61f29ca81bb0d3efcfaf3a6ddb8678fc10e4
Chris Foy <cfoy3@mit.edu>
2020-02-20 11:38:14 -0500

    Fixed merge

--- Modules/+Sources/+Signal_Generator/+SMIQ/SMIQ_PB_switch.m
+++ Modules/+Sources/+Signal_Generator/+SMIQ/SMIQ_PB_switch.m
@@ -2,12 +2,7 @@ classdef SMIQ_PB_switch < Sources.Signal_Generator.MW_PB_switch_invisible
     %SMIQ serial source class
     
     properties
-<<<<<<< HEAD
-        prefs = {'MWFrequency','MWPower','MW_switch_PB_line','SG_trig_PB_line','ip'};
-        
-=======
        prefs = {'MWFrequency','ip','MWPower','MW_switch_on','MW_switch_PB_line','SG_trig_PB_line'};
->>>>>>> PiezoCheck
     end
     
     properties(SetAccess=private, SetObservable)
@@ -18,10 +13,7 @@ classdef SMIQ_PB_switch < Sources.Signal_Generator.MW_PB_switch_invisible
         function obj = SMIQ_PB_switch()
             obj.serial = Drivers.SignalGenerators.SMIQ06B.instance(obj.SG_name); 
             obj.loadPrefs;
-<<<<<<< HEAD
-=======
             obj.MW_switch_on = 'yes';
->>>>>>> PiezoCheck
         end
     end
     
@@ -35,14 +27,5 @@ classdef SMIQ_PB_switch < Sources.Signal_Generator.MW_PB_switch_invisible
             obj = Object;
         end
     end
-<<<<<<< HEAD
-    
-    methods
-        function delete(obj)
-        end
-    end
-=======
-  
->>>>>>> PiezoCheck
 end
 
