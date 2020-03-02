bde9b5a6f7a2b2c3049f3fe74aed7206dbd30ec0
Eric Bersin <ebersin@mit.edu>
2020-03-02 15:31:56 -0500

    Removed extraneous functions.

--- HelperFunctions/PeakFitting/voigt.m
+++
@@ -1,5 +0,0 @@
-function y = voigt(x, A, x0, fl, fg)
-    [eta, f] = voigtApprox(fl, fg);
-
-    y = A * ( eta ./ (1 + (2*(x - x0)/f).^2) + (1-eta) * exp(- 2 * (sqrt(2*log(2)) * (x - x0) / f).^2) );
-end
