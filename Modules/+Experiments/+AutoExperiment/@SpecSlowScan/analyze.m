05a6906393688220d26305a7f44bc83b5394db95
Michael Walsh <mpwalsh@mit.edu>
2020-03-10 12:23:12 -0400

    Allow user to make analysis block and return analysis and fixed inds bug

--- Modules/+Experiments/+AutoExperiment/@SpecSlowScan/analyze.m
+++ Modules/+Experiments/+AutoExperiment/@SpecSlowScan/analyze.m
@@ -4,10 +4,15 @@ function varargout = analyze(data,varargin)
 %       data: data produced by GetData method
 %       [Analysis]: An analysis struct produced by this function
 %       [FitType]: "gauss", "lorentz", or "voigt" (default "gauss")
-%       [inds]: array of indices to mask full dataset (default: all data)
+%       [inds]: array of indices to mask full dataset (default: all data).
+%          This will also filter analysis if provided.
 %       [viewonly]: do not begin uifitpeaks on the axes
 %       [new]: arrow navigation will go to nearest new site (e.g. continued = 0)
-%   Outputs: None (see below)
+%       [block]: (false) Calls uiwait internally and will return analysis
+%          when user closes figure.
+%   Outputs:
+%       (fig): Figure handle
+%       (analysis): analysis struct (use with block=true).
 %   Interactivity:
 %       click on a spot to see corresponding data
 %       [alt+] left/right arrows to change site site_index. The alt is only
@@ -46,6 +51,7 @@ addParameter(p,'FitType','gauss',@(x)any(validatestring(x,{'gauss','lorentz','vo
 addParameter(p,'inds',1:length(data.data.sites),@(n)validateattributes(n,{'numeric'},{'vector'}));
 addParameter(p,'viewonly',false,@islogical);
 addParameter(p,'new',false,@islogical);
+addParameter(p,'block',false,@islogical);
 parse(p,varargin{:});
 
 prefs = data.meta.prefs;
@@ -54,9 +60,10 @@ data = FullData.data;
 im = data.image.image;
 sites = data.sites(p.Results.inds);
 
+
 fig = figure('name',mfilename,'numbertitle','off','CloseRequestFcn',@closereq);
 fig.Position(3) = fig.Position(3)*2;
-file_menu = findall(gcf,'tag','figMenuFile');
+file_menu = findall(fig,'tag','figMenuFile');
 uimenu(file_menu,'Text','Go to Index','callback',@go_to,'separator','on');
 uimenu(file_menu,'Text','Save Analysis','callback',@save_data);
 uimenu(file_menu,'Text','Export Analysis','callback',@export_data);
@@ -119,6 +126,7 @@ m(2).UserData.other = m(1);
 ax(5).UIContextMenu = c;
 
 % Constants and large structures go here
+block = p.Results.block;
 n = length(sites);
 filter_new = p.Results.new;
 viewonly = p.Results.viewonly;
@@ -173,6 +181,8 @@ if isstruct(p.Results.Analysis)
         new_data = true;
         warning('Added 4th column to analysis.sites')
     end
+    % Filter with inds
+    analysis.sites = analysis.sites(p.Results.inds,:);
 else
     analysis.nm2THz = [];
     analysis.gof = [];
@@ -191,8 +201,13 @@ end
 set([fig, selector],'KeyPressFcn',@cycleSite);
 update_all(); % Bypass changeSite since we have no previous site
 
+block = p.Results.block;
+if block
+    uiwait(fig);
+end
+
 if nargout
-    varargout = {fig};
+    varargout = {fig,analysis};
 end
 
     function open_diagnostic(varargin)
@@ -260,14 +275,17 @@ end
     end
 
     function closereq(~,~)
-        % Export data to workspace if analysis exists
-        try
-            if new_data
-                export_data(fig);
+        save_state();
+        if ~block % If block; we are returning data; assume no save expected
+            % Export data to workspace if analysis exists
+            try
+                if new_data
+                    export_data(fig);
+                end
+            catch err
+                delete(fig)
+                rethrow(err);
             end
-        catch err
-            delete(fig)
-            rethrow(err);
         end
         delete(fig)
     end
