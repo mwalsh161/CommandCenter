748d3fc4932cd3d32f65bf710e4113faa2812645
Ian Christen <ichr@mit.edu>
2020-09-18 19:11:46 -0400

    Added changed pref files.

--- +Prefs/+Inputs/BooleanField.m
+++ +Prefs/+Inputs/BooleanField.m
@@ -3,14 +3,32 @@ classdef BooleanField < Prefs.Inputs.LabelControlBasic
 
     properties(Hidden)
         uistyle = 'checkbox';
+        nanuistyle = 'radiobutton';
     end
     
     methods
         function set_value(obj,val)
-            obj.ui.Value = logical(val);
+            if isnan(val)
+                obj.ui.Value = false;
+                obj.ui.Style = obj.nanuistyle;
+                obj.ui.Enable = 'off';
+            else
+                obj.ui.Value = logical(val);
+                obj.ui.Style = obj.uistyle;
+                if obj.ui.UserData
+                    obj.ui.Enable = 'on';
+                else
+                    obj.ui.Enable = 'off';
+                end
+            end
         end
         function val = get_value(obj)
-            val = logical(obj.ui.Value);
+            switch obj.ui.Style
+                case obj.nanuistyle
+                    val = NaN;
+                case obj.uistyle
+                    val = logical(obj.ui.Value);
+            end
         end
     end
 
