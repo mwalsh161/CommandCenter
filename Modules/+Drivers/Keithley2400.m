a257188aa51a1b0677cd55ca5d81f415365dabb1
Ian Christen <ichr@mit.edu>
2021-04-08 22:03:51 -0400

    Reworked Keithley into the format of a CC Driver.

--- Modules/+Drivers/Keithley2400.m
+++ Modules/+Drivers/Keithley2400.m
@@ -1,20 +1,67 @@
-classdef Keithley2400 < handle
+classdef Keithley2400 < Modules.Driver
+    %KEITHLEY2400 Interfaces with the eponymous signal generator.
     
-    properties(SetAccess=protected)
+    properties (SetAccess=protected)
         GPIBAddr            % GPIB address
         RsrcName            % Resource name of the VISA instrument
         VisaHandle          % Handle of the VISA object
     end
-    methods%(Access=private)
+    
+    properties (GetObservable, SetObservable)
+        output =    Prefs.Boolean(false, 'set', 'set_output', ...
+                                                                'help_text', 'Whether the source is outputing voltage or current');
+        mode =      Prefs.MultipleChoice('VOLT', 'set', 'set_mode', 'choices', {'VOLT', 'CURR'}, ...
+                                                                'help_text', 'Output mode of the voltage/current source.');
+        voltage =   Prefs.Double(0, 'set', 'set_voltage', ...
+                                                                'help_text', 'Setpoint voltage of the source. This is different from measured voltage in the case the the source is operating in CURR mode.');
+        current =   Prefs.Double(0, 'set', 'set_current', ...
+                                                                'help_text', 'Setpoint current of the source. This is different from measured current in the case the the source is operating in VOLT mode.');
+    end
+    
+    % Constructor functions
+    methods (Static)
+        function obj = instance(GPIBAddr)
+%             mlock;
+            persistent Objects
+            if isempty(Objects)
+                Objects = Drivers.Keithley2400.empty(1,0);
+            end
+            for i = 1:length(Objects)
+                if isvalid(Objects(i)) && isequal(GPIBAddr,Objects(i).singleton_id)
+                    obj = Objects(i);
+                    return
+                end
+            end
+            obj = Drivers.Keithley2400(GPIBAddr);
+            obj.singleton_id = GPIBAddr;
+            Objects(end+1) = obj;
+        end
+    end
+    methods (Access=private)
+        function obj = Keithley2400(GPIBAddr)
+            obj.GPIBAddr = GPIBAddr;
+            obj.RsrcName = ['GPIB0::' num2str(obj.GPIBAddr) '::INSTR'];
+            obj.VisaHandle = visa('ni',obj.RsrcName);
+            obj.openConnection();
+        end
+    end
+    
+    % I/O functions
+    methods
         function openConnection(obj)
             if strcmp(obj.VisaHandle.Status,'closed')
                 fopen(obj.VisaHandle);
+                obj.init();     % Read values from the source.
             end
         end
         function closeConnection(obj)
             if strcmp(obj.VisaHandle.Status,'open')
                 fclose(obj.VisaHandle);
             end
+        end
+        function delete(obj)
+            obj.closeConnection();
+            delete(obj.VisaHandle);
         end
         function cmd(obj,msg)
             % open the port if closed
@@ -43,70 +90,59 @@ classdef Keithley2400 < handle
                 fclose(obj.VisaHandle);
             end
         end
+        function init(obj)
+            obj.output =    obj.get_output();
+            obj.mode =      obj.get_mode();
+            obj.voltage =   obj.get_voltage();
+            obj.current =   obj.get_current();
+        end
     end
+    
+    % Basic control functions
     methods
-        function obj = Keithley2400(GPIBAddr)
-            obj.GPIBAddr = GPIBAddr;
-            obj.RsrcName = ['GPIB0::' num2str(obj.GPIBAddr) '::INSTR'];
-            obj.VisaHandle = visa('ni',obj.RsrcName);
-            obj.openConnection();
+        % Output
+        function val = set_output(obj, val, ~)
+            val = logical(val);
+            obj.cmd(['outp:stat ' num2str(val)]);
         end
-        function delete(obj)
-            obj.closeConnection();
-            delete(obj.VisaHandle);
-        end
-        %% Basic control functions
-        function outputOn(obj)
-            obj.cmd('outp:stat 1');
-        end
-        function outputOff(obj)
-            obj.cmd('outp:stat 0');
-        end
-        function output = outputState(obj)  % 1 for On, 0 for Off
+        function output = get_output(obj)
             output = str2double(obj.query('outp:stat?'));
+            obj.output = output;
         end
-        function setOutputMode(obj,mode)    % mode can be: 'VOLT' or 'CURR'
-            obj.cmd(['sour:func:mode ' mode]);
+        
+        % Mode
+        function val = set_mode(obj, val, ~)
+            assert(strcmp(val, 'VOLT') || strcmp(val, 'CURR'), 'mode must be: ''VOLT'' or ''CURR''');
+            obj.cmd(['sour:func:mode ' val]);
         end
-        function setOutputVoltage(obj,volt)
-            obj.cmd(['sour:volt:lev:imm:ampl ' num2str(volt)]);
+%         function mode = get_mode(obj)
+        
+        % Setpoint voltage
+        function set_voltage(obj, val, ~)
+            obj.cmd(['sour:volt:lev:imm:ampl ' num2str(val)]);
         end
-        function voltOut = readOutputVoltage(obj) % gives set output voltage (output might be either on or off)
-            voltOut = str2double(obj.query('sour:volt?'));
+        function voltage = get_voltage(obj)
+            voltage = str2double(obj.query('sour:volt?'));
         end
-        function setOutputCurrent(obj,curr)
-            obj.cmd(['sour:curr:lev:imm:ampl ' num2str(curr)]); % gives set output current (output might be either on or off)
+        
+        % Setpoint current
+        function set_current(obj, val, ~)
+            obj.cmd(['sour:curr:lev:imm:ampl ' num2str(val)]);
         end
-        function currOut = readOutputCurrent(obj)
-            currOut = str2double(obj.query('sour:curr?'));
+        function current = get_current(obj)
+            current = str2double(obj.query('sour:curr?'));
         end
-        %% Measurement functions
-        % Measure voltage with an open circuit configuration (current = 0 A)
-        function voltMeas = measureVoltage(obj)
-            % turn I-source on (0 A: open circuit) and measure V
-            obj.setOutputCurrent(0);
-            obj.setOutputVoltage(0);
-            obj.setOutputMode('CURR');
-            obj.outputOn();
-            voltMeas = str2double(strtok(obj.query('meas:VOLT:DC?'),','));  % returned measuremnet consists of 5 numbers, the first is voltage
+        
+        % Measure voltage
+        function voltage = measureVoltage(obj)
+            voltage = str2double(strtok(obj.query('meas:VOLT:DC?'),','));  % returned measuremnet consists of 5 numbers, the first is voltage
         end
-        % Measure current with a previously set voltage
-        function currMeas = measureCurrent(obj)
-            % query output mode and whether output is on
-%             outputMode = obj.query('sour:func:mode?');
-%             currOut = readOutputCurrent(obj);
-%             outputOnDone = obj.outputState;
-            % turn V-source on and measure I
-%             obj.setOutputCurrent(0);
-%             obj.setOutputMode('VOLT');
-%             obj.outputOn();
+        
+        % Measure current
+        function current = measureCurrent(obj)
             queryOutput = obj.query('meas:CURR:DC?');
             [~,remain] = strtok(queryOutput,',');
-            currMeas = str2double(strtok(remain,','));  % returned measuremnet consists of 5 numbers, the second one is current
-            % return Keithley to state before measurement
-%             obj.setOutputCurrent(currOut);
-%             obj.cmd(['sour:func:mode ' outputMode]);
-%             obj.cmd(['outp:stat ' num2str(outputOnDone)]);  % return output to original state
+            current = str2double(strtok(remain,','));  % Returned measurement consists of 5 numbers, the second one is current
         end
     end
 end
