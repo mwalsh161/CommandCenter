d3d4b5e83e1faae5a4926d6ddcce88afdbe7307c
Ian Christen <ichr@mit.edu>
2021-05-06 23:10:11 -0400

    Fixes to .gds labels.

--- Modules/+Drivers/Conex_CC.m
+++ Modules/+Drivers/Conex_CC.m
@@ -9,9 +9,9 @@ classdef Conex_CC < Modules.Driver
         identifier =    Prefs.String('', 'readonly', true);
         state =         Prefs.String('UNKNOWN', 'readonly', true);
         
-        position =      Prefs.Double(NaN, 'unit', 'mm',     'min', 0, 'max', 25,        'allow_nan', true, 'set', 'set_position',       'help', 'Positon of the micrometer.');
-        velocity =      Prefs.Double(NaN, 'unit', 'mm/s',   'min', 0,        'allow_nan', true, 'set', 'set_velocity',       'help', 'Velocity of the micrometer.');
-        acceleration =  Prefs.Double(NaN, 'unit', 'mm/s^2', 'min', 1e-6, 'max', 1e12,   'allow_nan', true, 'set', 'set_acceleration',   'help', 'Acceleration of the micrometer.');
+        position =      Prefs.Double(NaN, 'unit', 'um',     'min', 0, 'max', 1e3*25,        'allow_nan', true, 'set', 'set_position',       'help', 'Positon of the micrometer.');
+        velocity =      Prefs.Double(NaN, 'unit', 'um/s',   'min', 0,        'allow_nan', true, 'set', 'set_velocity',       'help', 'Velocity of the micrometer.');
+        acceleration =  Prefs.Double(NaN, 'unit', 'um/s^2', 'min', 1e3*1e-6, 'max', 1e3*1e12,   'allow_nan', true, 'set', 'set_acceleration',   'help', 'Acceleration of the micrometer.');
     end
     properties % (Access=private)
         s;      % Handle to serial connection
@@ -50,17 +50,29 @@ classdef Conex_CC < Modules.Driver
         end
 
         function val = get_identifier(obj, ~)
-            obj.com('ID?');    % Get laser modulation power (mW)
-            
-            val = fscanf(obj.s);
+            if obj.isConnected()
+                obj.com('ID?');    % Get laser modulation power (mW)
+
+                val = fscanf(obj.s);
+            else
+                val = 'Not Connected';
+            end
         end
         function val = get_position(obj, ~)
-            obj.com('TH');    % Current postion
-            val = obj.recv();
+            if obj.isConnected()
+                obj.com('TH');    % Current postion
+                val = 1e3*obj.recv();
+            else
+                val = NaN;
+            end
         end
         function val = get_setpoint(obj, ~)
-            obj.com('PA?');    % Current postion
-            val = obj.recv();
+            if obj.isConnected()
+                obj.com('PA?');    % Setpoint postion
+                val = 1e3*obj.recv();
+            else
+                val = NaN;
+            end
         end
         function val = set_position(obj, val, ~)
             obj.com('ST');                  % Stop any current movement
@@ -74,21 +86,29 @@ classdef Conex_CC < Modules.Driver
             
 %             obj.com(['SE' num2str(val)]);   % Tell the axes to goto the desired position.
 %             fprintf(obj.s, 'SE');               
-            obj.com(['PA' num2str(val)]);   % Tell the axes to goto the desired position.
+            obj.com(['PA' num2str(val/1e3)]);   % Tell the axes to goto the desired position.
         end
         function val = set_velocity(obj, val, ~)
-            obj.com(['VA' num2str(val)]);   % Tell the axes to goto the desired position.
+            obj.com(['VA' num2str(val/1e3)]);   % Tell the axes to goto the desired position.
             val = get_velocity(obj);
         end
         function val = get_velocity(obj, ~)
-            obj.com('VA?');  val = obj.recv();
+            if obj.isConnected()
+                obj.com('VA?');  val = 1e3*obj.recv();
+            else
+                val = NaN;
+            end
         end
         function val = set_acceleration(obj, val, ~)
-            obj.com(['AC' num2str(val)]);   % Tell the axes to goto the desired position.
+            obj.com(['AC' num2str(val/1e3)]);   % Tell the axes to goto the desired position.
             val = get_acceleration(obj);
         end
         function val = get_acceleration(obj, ~)
-            obj.com('AC?'); val = obj.recv();
+            if obj.isConnected()
+                obj.com('AC?'); val = 1e3*obj.recv();
+            else
+                val = NaN;
+            end
         end
         function [state, err] = get_raw_state(obj, ~)
             obj.com('TS');   % Tell the axes to goto the desired position.
@@ -104,7 +124,11 @@ classdef Conex_CC < Modules.Driver
             end
         end
         function state = get_state(obj, ~)
-            [raw, ~] = get_raw_state(obj, 0);
+            try
+                [raw, ~] = get_raw_state(obj, 0);
+            catch
+                raw = '';
+            end
             
 %             switch raw
 %                 case '0A'; state = 'RESET -> NOT REFERENCED';
@@ -169,12 +193,15 @@ classdef Conex_CC < Modules.Driver
             val = str2double(str(4:end));
         end
         
+        function tf = isConnected(obj)
+            tf = ~isempty(obj.s) && isvalid(obj.s);
+        end
+        
         function val = set_host(obj, val, ~) %this loads the hwserver driver
             delete(obj.s);
             
             if strcmp('COM?', val)
                 obj.s = [];
-                obj.diode_on = false;
                 return
             end
             err = [];
