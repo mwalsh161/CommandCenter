174c19ac9a712ae1445b73f83eeccea0a22a6fc4
Ian Christen <ichr@mit.edu>
2021-06-10 00:44:18 -0400

    Fixed a latent crash due to improperly initialized UserData in an Inputs.FileField.

--- +Prefs/File.m
+++ +Prefs/File.m
@@ -65,7 +65,7 @@ classdef File < Base.pref
     methods(Hidden) % Callback
         function select_file(obj,hObj,eventdata,~)
             if ~isfield(hObj.UserData,'last_choice')
-                hObj.UserData.last_choice = obj.value;
+                hObj.UserData = struct('last_choice', obj.value);
             end
             name = sprintf('Select File: %s',obj.name);
             [file,path] = uigetfile(obj.filter_spec,name,hObj.UserData.last_choice);
