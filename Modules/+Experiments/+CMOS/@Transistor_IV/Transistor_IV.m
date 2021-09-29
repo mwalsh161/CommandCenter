2428b991c9f026d381202c84dd2f587017f60915
cassie-song <songyixuan1998@gmail.com>
2021-09-29 16:19:35 -0400

    Added settle time; fixed some bugs

--- Modules/+Experiments/+CMOS/@Transistor_IV/Transistor_IV.m
+++ Modules/+Experiments/+CMOS/@Transistor_IV/Transistor_IV.m
@@ -2,22 +2,31 @@ classdef Transistor_IV < Modules.Experiment
     % CMOS_Rabi Performs a rabi measurement with a MW drive, and a
     % single laser to initialize and readout
 
-    properties(SetObservable,AbortSet)
+    properties(SetObservable,GetObservable,AbortSet)
         % Modules
-        Vgs_Power_Supply = Prefs.ModuleInstance('help_text','Power supply for gate voltage');
+        Vgs_supply = Prefs.ModuleInstance('help_text','Power supply for gate voltage');
         Vgs_channel = Prefs.Integer(1,'help_text','Channel to use on Vgs power supply','min',1)
-        Vds_Power_Supply = Prefs.ModuleInstance('help_text','Power supply for drain voltage');
+        Vds_supply = Prefs.ModuleInstance('help_text','Power supply for drain voltage');
         Vds_channel = Prefs.Integer(1,'help_text','Channel to use on Vds power supply','min',1)
 
         % Sweep parameters
         Vgs = Prefs.String('help_text','Evaluates to voltages to apply to the gate','units','V','set','set_Vgs');
+        Vgs_I_limit = Prefs.Double(.01,'help_text','Current Limit for Vgs','units','A');
         Vds = Prefs.String('help_text','Evaluates to voltages to apply to the drain','units','V','set','set_Vds');
+        Vds_I_limit = Prefs.Double(.01,'help_text','Current Limit for Vds','units','A');
+        settle_time = Prefs.Double(100,'help_text','Time to wait before measuring current','units','ms')
 
         % Cosmetic
         x_axis_is_Vgs = Prefs.Boolean(false, 'help_text','Whether to plot Vgs or Vds on x-axis (sweep of the other value will show up as multiple lines)')
     end
     properties
-        prefs = {'Vds','Vgs','x_axis_is_Vgs','Vds_Power_Supply','Vds_channel','Vgs_Power_Supply','Vgs_channel'}
+        prefs = {'Vds','Vds_I_limit','Vgs','Vgs_I_limit','settle_time','x_axis_is_Vgs','Vds_supply','Vds_channel','Vgs_supply','Vgs_channel'}
+    end
+    properties(SetAccess=private,Hidden)
+        % Internal properties that should not be accessible by command line
+        data = [] % Useful for saving data from run method
+        meta = [] % Useful to store meta data in run method
+        abort_request = false; % Flag that will be set to true upon abort. Use in run method!
         vgs_vals = []; % Internal, set using Vgs pref
         vds_vals = []; % Internal, set using Vds pref
     end
@@ -33,18 +42,28 @@ classdef Transistor_IV < Modules.Experiment
     methods
         run(obj,status,managers,ax) % Main run method in separate file
 
-        function val = set_Vgs(val,~)
-            vals = str2num(val);
-            assert(isvector(vals),'Vgs should evaluate to a vector')
-            vgs_vals = vals;
-            val = vals;
+        function dat = GetData(obj,stageManager,imagingManager)
+            % Callback for saving methods
+            meta = stageManager.position;
+            dat.data = obj.data;
+            dat.meta = obj.meta;
         end
 
-        function val = set_Vds(val,~)
+        function abort(obj)
+            % Callback for when user presses abort in CC
+            obj.abort_request = true;
+        end
+
+        function val = set_Vgs(obj,val,~)
+            vals = str2num(val);
+            assert(isvector(vals),'Vgs should evaluate to a vector')
+            obj.vgs_vals = vals;
+        end
+
+        function val = set_Vds(obj,val,~)
             vals = str2num(val);
             assert(isvector(vals),'Vds should evaluate to a vector')
-            vds_vals = vals;
-            val = vals;
+            obj.vds_vals = vals;
         end
     end
 end
--- Modules/+Experiments/+CMOS/@Transistor_IV/run.m
+++ Modules/+Experiments/+CMOS/@Transistor_IV/run.m
@@ -11,41 +11,47 @@ function run( obj,status,managers,ax )
     obj.meta.Vds = obj.vds_vals;
 
     % Prepare plotting
-    N_Vgs = max(size(obj.vgs_vals);
-    N_Vds = max(size(obj.vds_vals);
-    obj.data = NaN(max(size(N_Vds)),max(size(N_Vgs)));
+    N_Vgs = max(size(obj.vgs_vals));
+    N_Vds = max(size(obj.vds_vals));
+    obj.data = NaN(N_Vds,N_Vgs);
 
     if obj.x_axis_is_Vgs
-        plotH = plot(obj.vgs_vals, obj.data,'parent',ax)
+        plotH = plot(obj.vgs_vals, obj.data,'parent',ax);
+        xlabel('Vgs (V)','parent',ax)
     else
-        plotH = plot(obj.vds_vals, obj.data,'parent',ax)
+        plotH = plot(obj.vds_vals, obj.data,'parent',ax);
+        xlabel('Vds (V)','parent',ax)
     end
-
+    ylabel('Id (A)','parent',ax)
+    
     % Initialise power supplies to first value and turn on
-    obj.Vgs_Power_Supply.Channel = obj.Vgs_channel;
-    obj.Vgs_Power_Supply.changeSource_Mode = 'Voltage';
-    obj.Vgs_Power_Supply.Voltage = obj.vgs_vals(1);
+    obj.Vgs_supply.Channel = obj.Vgs_channel;
+    obj.Vgs_supply.changeSource_Mode('Voltage');
+    obj.Vgs_supply.Current = obj.Vgs_I_limit;
+    obj.Vgs_supply.Voltage = obj.vgs_vals(1);
 
-    obj.Vds_Power_Supply.Channel = obj.Vds_channel;
-    obj.Vds_Power_Supply.changeSource_Mode = 'Voltage';
-    obj.Vds_Power_Supply.Voltage = obj.vds_vals(1);
+    obj.Vds_supply.Channel = obj.Vds_channel;
+    obj.Vds_supply.changeSource_Mode('Voltage');
+    obj.Vds_supply.Current = obj.Vds_I_limit;
+    obj.Vds_supply.Voltage = obj.vds_vals(1);
 
-    obj.Vgs_Power_Supply.on;
-    obj.Vds_Power_Supply.on;
+    obj.Vgs_supply.on;
+    obj.Vds_supply.on;
 
     try
 
         for j = 1:N_Vgs
             for i = 1:N_Vds
-                status.String = sprintf('Experiment started\nV_{gs} %i/%i, V_{gs} %i/%i, (V_{gs}=%.02f)',j,N_Vgs,obj.vds_vals(j));
+                status.String = sprintf('Experiment started\nVgs %i/%i (Vgs=%.02f)',j,N_Vgs,obj.vgs_vals(j));
                 drawnow; assert(~obj.abort_request,'User aborted.');
 
                 % Change voltages
-                obj.Vds_Power_Supply.Voltage = obj.vds_vals(i);
-                obj.Vgs_Power_Supply.Voltage = obj.vgs_vals(j);
+                obj.Vds_supply.Voltage = obj.vds_vals(i);
+                obj.Vgs_supply.Voltage = obj.vgs_vals(j);
 
                 % Get current
-                obj.data(i,j) = obj.Vds_Power_Supply.getCurrent(true);
+                pause(obj.settle_time/1000)
+                obj.data(i,j) = obj.Vds_supply.getCurrent(true);
 
                 % Update plots
                 if obj.x_axis_is_Vgs
@@ -59,6 +65,9 @@ function run( obj,status,managers,ax )
     catch err
     end
     % CLEAN UP CODE %
+    obj.Vgs_supply.off;
+    obj.Vds_supply.off;
+    
     if exist('err','var')
         % HANDLE ERROR CODE %
         rethrow(err)
