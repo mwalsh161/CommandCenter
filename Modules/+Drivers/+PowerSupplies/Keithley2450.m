96216e03d0c59c160d027164c23a310cbce63e20
cassie-song <songyixuan1998@gmail.com>
2021-10-23 11:48:25 -0400

    Keithley 2450 source/driver fixed

--- Modules/+Drivers/+PowerSupplies/Keithley2450.m
+++ Modules/+Drivers/+PowerSupplies/Keithley2450.m
@@ -55,37 +55,27 @@ classdef Keithley2450 < Modules.Driver
         end
     end
     
-    methods(Access=private)
+    methods
         
         function  setMeasurement_Mode(obj,measurement_type)
             switch measurement_type
                 case {'Current','Curr','curr','current'}
-                    string = sprintf('SENS:FUNC Curr ');
-                    obj.writeOnly(string);
+                    obj.writeOnly('SENS:FUNC "CURR"');
+                    obj.writeOnly('SENS:CURR:RANG:AUTO ON');
+                    obj.writeOnly('SENS:CURR:UNIT AMP');
+                    obj.writeOnly('SENS:CURR:OCOM ON');
                 case {'Voltage','Volt','volt','voltage'}
-                    string = sprintf('SENS:FUNC Volt ');
-                    obj.writeOnly(string);
+                    obj.writeOnly('SENS:FUNC "VOLT"');
+                    obj.writeOnly('SENS:VOLT:RANG:AUTO ON');
+                    obj.writeOnly('SENS:VOLT:UNIT VOLT');
+                    obj.writeOnly('SENS:VOLT:OCOM ON');
                 case {'Resistance','Res','res','resistance'}
-                    string = sprintf('SENS:FUNC Res ');
-                    obj.writeOnly(string);
+                    obj.writeOnly('SENS:FUNC "RES"');
+                    obj.writeOnly('SENS:RES:RANG:AUTO ON');
+                    obj.writeOnly('SENS:RES:UNIT OHM');
+                    obj.writeOnly('SENS:RES:OCOM ON');
                 otherwise
                     error([measurement_type,' is not a supported measurement type!'])
-            end
-        end
-        
-        function value=Measure(obj)
-            string = sprintf('FETC?');
-            s = obj.writeRead(string);
-            value = str2num(s);
-        end
-        
-        function  setTrigger_type(obj,Trigger_type)
-            switch Trigger_type
-                case 'IMM'
-                    string = sprintf(':TRIG:SOUR IMM');
-                    obj.writeOnly(string);
-                otherwise
-                    error('Only Immediate (Imm) trigger allowed!')
             end
         end
         
@@ -97,34 +87,13 @@ classdef Keithley2450 < Modules.Driver
             output = query(obj.comObject,string);
         end
         
-        function state = current_limit_state(obj)
-            string = sprintf(':SOURce:CURRent:PROTection?');
-            s = obj.writeRead(string);
-            current_limit_setting = s(1:end-1);
-            if current_limit_setting == '1'
-                state = 'on';
-            else
-                state = 'off';
-            end
-        end
-        
-        function state = voltage_limit_state(obj)
-            string = sprintf(':SOURce:Voltage:PROTection?');
-            s = obj.writeRead(string);
-            voltage_limit_setting = s(1:end-1);
-            if voltage_limit_setting == '1'
-                state = 'on';
-            else
-                state = 'off';
-            end
-        end
-        
         function testLimit(obj,sourceMode)
+            sourceMode = upper(sourceMode);
             switch sourceMode
-                case {'current','Current','CURRENT'}
+                case 'CURRENT'
                     [upperLimit,lowerLimit] = obj.getVoltageLimit('1');
                     measured_value = obj.measureVoltage('1');
-                case {'voltage','VOLTAGE','Voltage'}
+                case 'VOLTAGE'
                     [upperLimit,lowerLimit] = obj.getCurrentLimit('1');
                     measured_value = obj.measureCurrent('1');
                 otherwise
@@ -141,17 +110,15 @@ classdef Keithley2450 < Modules.Driver
                 warndlg([obj.dev_id,'''s channel 1 is railing against its ',sourceMode,' limit'])
             end
         end
-    end
-    
-    methods    
+
         function check_channel(obj,channel)
             assert(ischar(channel),'Channel input must be a string!')
-            assert(strcmp(channel,'1'),'Yokogawa only supports channel inputs of ''1''!')
+            assert(strcmp(channel,'1'),'Keithley 2450 only supports channel inputs of ''1''!')
         end
         
         function setVoltageRange(obj,channel,range)
             obj.check_channel(channel)
-            possibleValues = num2cell([110,60,30,20,12,2,0.2]);
+            possibleValues = num2cell([]);
             switch range
                 case {'Auto','auto','AUTO'}
                     string = sprintf(':SOURce:Volt:RANGE:AUTO 1');
@@ -169,7 +136,7 @@ classdef Keithley2450 < Modules.Driver
         
         function setCurrentRange(obj,channel,range)
             obj.check_channel(channel)
-            possibleValues = num2cell([3,2,1,0.5,0.2,0.02,0.002,0.0002,0.00002]);
+            possibleValues = num2cell([]);
             switch range
                 case {'Auto','auto','AUTO'}
                     string = sprintf(':SOURce:Current:RANGE:AUTO 1');
@@ -189,7 +156,7 @@ classdef Keithley2450 < Modules.Driver
             obj.check_channel(channel)
             assert(isnumeric(current),'current must be data type numeric')
             obj.setSourceMode(channel,'Current');
-            string = sprintf(':SOURce:CURRent:LEVel %f',current);
+            string = sprintf(':SOUR:CURR %f',current);
             obj.writeOnly(string);
             pause(obj.pauseTime)
         end
@@ -198,7 +165,7 @@ classdef Keithley2450 < Modules.Driver
             obj.check_channel(channel)
             assert(isnumeric(voltage),'voltage must be data type numeric')
             obj.setSourceMode(channel,'Voltage');
-            string = sprintf(':SOURce:Voltage:LEVel %f',voltage);
+            string = sprintf(':SOUR:VOLT %f',voltage);
             obj.writeOnly(string);
             pause(obj.pauseTime)
         end
@@ -211,7 +178,7 @@ classdef Keithley2450 < Modules.Driver
                 warning([obj.dev_id,' only supports symmetrical voltage limits. Voltage limit'...
                     ' will be set to +/- ',num2str(abs(volt_limit(1))),' volts.']);
             end
-            string = sprintf(':SOURce:VOLT:PROT:ULIM %f', abs(volt_limit(1)));
+            string = sprintf(':SOUR:CURR:VLIM %f', abs(volt_limit(1)));
             obj.writeOnly(string);
             obj.setSourceMode(channel,'Current');
             pause(obj.pauseTime)
@@ -225,7 +192,7 @@ classdef Keithley2450 < Modules.Driver
                 warning([obj.dev_id,' only supports symmetrical current limits. Current limit'...
                     ' will be set to +/- ',num2str(abs(current_limit(1))),' amps.']);
             end
-            string = sprintf(':SOURce:CURR:PROT:ULIM %f', abs(current_limit(1)));
+            string = sprintf(':SOUR:VOLT:ILIM %f', abs(current_limit(1)));
             obj.writeOnly(string);
             obj.setSourceMode(channel,'Voltage');
             pause(obj.pauseTime)
@@ -233,13 +200,13 @@ classdef Keithley2450 < Modules.Driver
         
         function  setSourceMode(obj,channel,source_type)
             obj.check_channel(channel)
-            switch lower(source_type)
-                case 'current'
-                    string = sprintf(':SOURce:FUNCtion CURRent');
-                    obj.writeOnly(string);
-                case 'voltage'
-                    string = sprintf(':SOURce:FUNCtion VOLT');
-                    obj.writeOnly(string);
+            switch upper(source_type)
+                case 'CURRENT'
+                    obj.writeOnly(':SOUR:FUNC CURR');
+                    obj.setMeasurement_Mode('Voltage')
+                case 'VOLTAGE'
+                    obj.writeOnly(':SOUR:FUNC VOLT');
+                    obj.setMeasurement_Mode('Current')
                 otherwise
                     error([source_type,' is not a supported source type!'])
             end
@@ -250,90 +217,56 @@ classdef Keithley2450 < Modules.Driver
         
         function  [voltage] = measureVoltage(obj,channel)
             obj.check_channel(channel)
-            obj.setMeasurement_Mode('Voltage');
-            voltage=obj.Measure;
-            if abs(voltage) > 1e5
-                pause(obj.pauseTime) %not sure why I need this
-                voltage=obj.Measure;
-            end
+            voltage = obj.writeRead(':MEAS:VOLT?');
+            voltage = str2double(voltage);
         end
+
         
         function  [current] = measureCurrent(obj,channel)
             obj.check_channel(channel)
-            obj.setMeasurement_Mode('Current');
-            current=obj.Measure;
-            if abs(current) > 1e5
-              pause(obj.pauseTime) %not sure why I need this
-              current=obj.Measure;
-            end
-        end
-        %%
-        
-        function  [Current_Range]=getCurrentRange(obj,channel)
-            obj.check_channel(channel)
-            %test if AUTO
-            string = sprintf(':SOURce:Current:RANGE:AUTO?');
-            s = obj.writeRead(string);
-            if ~s(1:end-1)
-                string = sprintf(':SOURce:CURRent:RANGe?');
-                s = obj.writeRead(string);
-                Current_Range = str2num(s(1:end-1));
-            else
-                Current_Range = 'Auto';
-            end
-        end
-        
-        function [Volt_Range]=getVoltageRange(obj,channel)
-            obj.check_channel(channel)
-            %test if AUTO
-            string = sprintf(':SOURce:Voltage:RANGE:AUTO?');
-            s = obj.writeRead(string);
-            if ~s(1:end-1)
-                string = sprintf(':SOURce:Voltage:RANGe?');
-                s = obj.writeRead(string);
-                Volt_Range = str2num(s(1:end-1));
-            else
-                Volt_Range = 'Auto';
-            end
+            current = obj.writeRead(':MEAS:CURR?');
+            current = str2double(current);
         end
         
         function current = getCurrent(obj,channel)
+            % Get set value
             obj.check_channel(channel)
             obj.setSourceMode(channel,'Current');
-            string = sprintf(':SOURce:CURRent:LEVel?');
+            string = sprintf(':SOUR:CURR:LEV?');
             s = obj.writeRead(string);
-            current = str2num(s(1:end-1));
+            current = str2double(s(1:end-1));
         end
         
         function voltage = getVoltage(obj,channel)
+            % Get set value
             obj.check_channel(channel)
             obj.setSourceMode(channel,'Voltage');
-            string = sprintf(':SOURce:Voltage:LEVel?');
+            string = sprintf(':SOUR:CURR:LEV?');
             s = obj.writeRead(string);
-            voltage = str2num(s(1:end-1));
+            voltage = str2double(s(1:end-1));
         end
         
         function [upperlim,lowerlim]  = getCurrentLimit(obj,channel)
             obj.check_channel(channel)
-            string = sprintf(':SOUR:CURR:PROT:ULIM?');
+            string = sprintf(':SOUR:VOLT:ILIM?');
             s = obj.writeRead(string);
-            Current_Limit = str2num(s(1:end-1));
+            Current_Limit = str2double(s(1:end-1));
             upperlim = Current_Limit;
             lowerlim = -Current_Limit;
         end
         
         function [upperlim,lowerlim] = getVoltageLimit(obj,channel)
             obj.check_channel(channel)
-            string = sprintf(':SOURce:VOLTAGE:PROT:ULIM?');
+            string = sprintf(':SOUR:CURR:VLIM?');
             s = obj.writeRead(string);
-            VoltLim = str2num(s(1:end-1));
+            VoltLim = str2double(s(1:end-1));
             upperlim = VoltLim;
             lowerlim = -VoltLim;
         end
         
         function sourceMode = getSourceMode(obj,channel)
             obj.check_channel(channel)
-            string = sprintf(':SOURce:FUNCtion?');
+            string = sprintf(':SOUR:FUNC?');
             sourceMode = obj.writeRead(string);
             sourceMode = sourceMode(1:end-1);
             if strcmp(sourceMode,'VOLT')
@@ -343,16 +276,11 @@ classdef Keithley2450 < Modules.Driver
             end
         end
         
-        function [Power_supply_state]  = getState(obj,channel)
+        function state  = getState(obj,channel)
             obj.check_channel(channel)
             string = sprintf(':OUTP:STAT?');
             s = obj.writeRead(string);
-            state = str2num(s(1:end-1));
-            if state==0
-                Power_supply_state = 'Off';
-            else
-                Power_supply_state = 'On';
-            end
+            state = str2double(s(1:end-1));
         end
         
         %%
@@ -367,14 +295,8 @@ classdef Keithley2450 < Modules.Driver
             else
                 channel = '1';
             end
-            curr_lim_state = obj.current_limit_state;
-            volt_lim_state = obj.voltage_limit_state;
-            if strcmpi(curr_lim_state,'On') && strcmpi(volt_lim_state,'On')
-                string = sprintf(':OUTP:STAT 1 ');
-                obj.writeOnly(string);
-            else
-                error('you need to have a voltage limit and a current limit set to turn on.')
-            end
+            
+            obj.writeOnly(':OUTP:STAT 1 ');
             %check if your hitting a device limit
             sourceMode = obj.getSourceMode(channel);
             obj.testLimit(sourceMode);
@@ -384,7 +306,7 @@ classdef Keithley2450 < Modules.Driver
         function off(obj,varargin)
             narginchk(1,2)
             if nargin>1
-                warning('Yokogawa does not support selective channel turning off. All channels will turn off.')
+                warning('Keithley 2450 does not support selective channel turning off. All channels will turn off.')
             end
             string = sprintf(':OUTP:STAT 0 ');
             obj.writeOnly(string);
@@ -392,7 +314,7 @@ classdef Keithley2450 < Modules.Driver
         
         function delete(obj)
             obj.reset;
-            string = sprintf('SYSTEM:LOCAL '); %set the supply back to local control
+            %string = sprintf('SYSTEM:LOCAL '); %set the supply back to local control
             obj.writeOnly(string);
             fclose(obj.comObject);
             delete(obj.comObject);
@@ -400,13 +322,7 @@ classdef Keithley2450 < Modules.Driver
         
         function reset(obj)
             obj.off;
-            string = sprintf('*RST');
-            obj.writeOnly(string);
-            obj.setVoltageLimit('1',5);
-            obj.setCurrentLimit('1',0.05);
-            obj.setCurrentRange('1','Auto')
-            obj.setVoltageRange('1','Auto')
-            obj.setTrigger_type('IMM')
+            obj.writeOnly('*RST');
         end
     end
 end
--- Modules/+Sources/+PowerSupplies/Keithley2450_Source.m
+++ Modules/+Sources/+PowerSupplies/Keithley2450_Source.m
@@ -2,9 +2,8 @@ classdef Keithley2450_Source < Sources.PowerSupplies.PowerSupply_invisible
     % Keithley 2450 power supply source
 
     properties(SetObservable,GetObservable,AbortSet)
-        Com_Address = 'NONE'; % COM address prologix connection. Is 'NONE' if no connection is desired.
-        Primary_Address = 'NONE'; % Primary address for prologix connection. Is 'NONE' if no connection is desired.
-        Channel = Prefs.MultipleChoice('1','allow_empty',false,'choices',{'1'},'help_text','Power supply channel to change','set','changeChannel');
+        VISA_address = Prefs.String('NONE','set','set_VISA_address'); % COM address prologix connection. Is 'NONE' if no connection is desired.
+        Channel = Prefs.MultipleChoice('1','allow_empty',false,'choices',{'1'},'help_text','Power supply channel to change');
         Currents = [.05]; % Memory of what all the voltages are to be saved in prefs
         Voltages = [1]; % Memory of what all the currents are to be saved in prefs
         SourceModes = {'Voltage'} % Memory of what all the Source_Modes are to be saved in prefs
@@ -21,47 +20,9 @@ classdef Keithley2450_Source < Sources.PowerSupplies.PowerSupply_invisible
     
     methods(Access=protected)
         function obj = Keithley2450_Source()
-            obj.connectSerial( obj.Com_Address, obj.Primary_Address );
-            obj.prefs = {obj.prefs{:},'Com_Address','Primary_Address'};
-            obj.show_prefs = {obj.show_prefs{:},'Com_Address','Primary_Address'};
+            obj.prefs = {obj.prefs{:},'VISA_address'};
+            obj.show_prefs = {obj.show_prefs{:},'VISA_address'};
             obj.loadPrefs;
-        end
-
-        function success = connectSerial(obj, Com_Address, Primary_Address)
-             % If using the default non-existant Com_Address, disconnect device
-             Com_Address = upper(Com_Address);
-             Primary_Address = upper(Primary_Address);
-             if strcmp(Com_Address,'NONE') || strcmp(Primary_Address,'NONE')
-                 delete(obj.power_supply);
-                 obj.power_supply = [];
-                 obj.power_supply_connected=false;
-                 success = true;
-                 return
-             end
-             
-             Primary_Address = str2num(Primary_Address);
-             
-             % Otherwise try to connect with input Com and instantiate power_supply
-             oldSerial = obj.power_supply;
-             try
-                 serial_device = prologix(Com_Address,Primary_Address);
-                 obj.power_supply = Drivers.PowerSupplies.Keithley2450.instance(obj.Power_Supply_Name,serial_device);
-                 delete(oldSerial);
-                 obj.power_supply_connected=true;
-                 success = true;
-             catch ME
-                 err_message = sprintf('Failed to open device. Error message:\n%s\nMessage identifier:\n%s', ME.message, ME.identifier);
-                 f = msgbox(err_message);
-                 
-                 % If connection fails, keep old instance of power_supply
-                 % and delete serial device
-                 delete(serial_device);
-                 obj.power_supply = oldSerial;
-                 obj.power_supply_connected=false;
-                 success = false;
-             end
-             
-             obj.updatePrefs(); % Update values if connection is successful
         end
     end
     
@@ -78,25 +39,6 @@ classdef Keithley2450_Source < Sources.PowerSupplies.PowerSupply_invisible
     
     methods
         % If the Com_Address changed, need to attempt to reconnect device with new info
-        function set.Com_Address(obj,val)
-            val = upper(val);
-            success  = obj.connectSerial(val, obj.Primary_Address);
-            
-            if success
-                obj.Com_Address = val;
-            end
-        end
-        function set.Primary_Address(obj,val)
-            val = upper(val);
-            success  = obj.connectSerial(obj.Com_Address, val);
-            
-            if success
-                obj.Primary_Address = val;
-            end
-        end
-        function val = changeChannel(obj,val,pref)
-            obj.updateValues();
-        end
         function arm(obj)
         end
         
@@ -154,6 +96,55 @@ classdef Keithley2450_Source < Sources.PowerSupplies.PowerSupply_invisible
                     val = obj.queryPowerSupply('getVoltageLimit',obj.Channel);%if the source isn't on return the programmed values
                     obj.Currents(obj.getHWIndex(obj.Channel)) = val;
                 end
+            end
+        end
+
+        function connectVISA(obj, address)
+            try
+                comObjectArray = instrfind('Type', 'visa-usb', 'RsrcName', address, 'Tag', '');
+                
+                % Create the VISA-USB object if it does not exist
+                % otherwise use the object that was found.
+                if isempty(comObjectArray)
+                    comObject = visa('NI', address);
+                else
+                    if iscell(comObjectArray)
+                        for index = 1:numel(comObjectArray)
+                            comObject = comObjectArray{index};
+                            if strcmpi(comObject.status,'open')
+                                break
+                            end
+                            if index == numel(comObjectArray)
+                                fclose(comObjectArray);
+                                comObject = comObjectArray(1);
+                            end
+                        end
+                    else
+                        comObject = comObjectArray(1);
+                    end
+                end
+                obj.power_supply = Drivers.PowerSupplies.Keithley2450.instance(obj.Power_Supply_Name,comObject);
+                obj.power_supply_connected = true;
+            catch err
+                % Reset to NONE if connection failed
+                obj.power_supply_connected = false;
+                rethrow(err)
+            end
+        end
+        
+        function val = set_VISA_address(obj,val,~)
+            new_val = upper(val);
+
+            if ~strcmp(new_val,'NONE')
+                try
+                    obj.connectVISA(new_val)
+                    val = new_val;
+                catch err
+                    val = 'NONE';
+                end
+            else
+                obj.power_supply_connected = false;
+                val = new_val;
             end
         end
     end
