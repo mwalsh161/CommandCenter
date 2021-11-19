f4f18b20f1b8fe5b18a3d78bed1d4ee0ddb8e7db
ichr <ichr@mit.edu>
2021-11-18 23:15:07 -0500

    Code for measuring initialization probability with blue is working, code using the counter still needs to be completed

--- Modules/+Experiments/+PulseSequenceSweep/@InitializationBlueCounter/InitializationBlueCounter.m
+++ Modules/+Experiments/+PulseSequenceSweep/@InitializationBlueCounter/InitializationBlueCounter.m
@@ -1,19 +1,27 @@
-classdef TransitionRatesCounter < Experiments.PulseSequenceSweep.PulseSequenceSweep_invisible
-    %OpticalSpinPolarization measures the time dependence of the PLE signal
+classdef InitializationBlueCounter < Experiments.PulseSequenceSweep.PulseSequenceSweep_invisible
+    % Study the probability of initialization as a function of blue laser power with blue repump
 
     properties(SetObservable,GetObservable, AbortSet)
         resLaser = Modules.Source.empty(1,0); % Allow selection of source
         repumpLaser = Modules.Source.empty(1,0);
+        
+        keithley = Drivers.Keithley2400.instance(0,16);
+        arduino = Drivers.ArduinoServo.instance('localhost', 2);
+        counter = Drivers.Counter.instance('APD1','CounterSync');
+        
+        % KeithleyVolatge = 3; % Voltage for keithley to control the laser power
         APDline = 3;
-        repumpTime_us = 1; %us
+        repumpTime_us = 1000; %us
         resOffset_us = 0.1;
-        resTime_us = 10;
+        resTime_us = 10000;
+        
+        est_CountsPerSecond_cps = 20000;
         
         resLaserPower_ang = 0; % angle on filter wheel, might need to calibrate the corresponding laser power
         repumpLaserPower_mW = 1;
         
-        resLaserPower_range = 'linspace(0,180,101)';
-        repumpLaserPower_range = 'linspace(0.1,10,101)';
+        resLaserPower_range = 'linspace(0,180,101)'; 
+        repumpLaserPower_range = 'linspace(0.1,10,101)'; % voltage for keithley
         
     end
     properties
@@ -21,7 +29,7 @@ classdef TransitionRatesCounter < Experiments.PulseSequenceSweep.PulseSequenceSw
         counterDuration = 0; %calculated in set.resTime_us
     end
     properties(Constant)
-        nCounterBins = 1; %number of APD bins for this pulse sequence (with more than 20 the PB errors)
+        nCounterBins = 20; %number of APD bins for this pulse sequence (with more than 20 the PB errors)
         counterSpacing = 0.1; %spacing between APD bins
         vars = {'resLaserPower_range', 'repumpLaserPower_range'}; %names of variables to be swept
         
@@ -30,7 +38,7 @@ classdef TransitionRatesCounter < Experiments.PulseSequenceSweep.PulseSequenceSw
         obj = instance()
     end
     methods(Access=private)
-        function obj = TransitionRatesCounter()
+        function obj = InitializationBlueCounter()
             obj.prefs = [obj.prefs,{'resLaser','APDline','repumpTime_us','repumpLaser','resOffset_us',...
             'resTime_us', 'resLaserPower_ang', 'repumpLaserPower_mW', 'resLaserPower_range', 'repumpLaserPower_range'}]; %additional preferences not in superclass
             obj.loadPrefs;
@@ -41,17 +49,21 @@ classdef TransitionRatesCounter < Experiments.PulseSequenceSweep.PulseSequenceSw
         pulseSeq = BuildPulseSequence(obj) %Defined in separate file
         
         function PreRun(obj,~,~,ax)
-            obj.repumpLaser.on
+%             obj.repumpLaser.on
+
+            obj.arduino.angle = obj.resLaserPower_ang; % initialize the resonant laser power
+            obj.keithley.set_voltage(0) % set the voltage level for keithley for blue laser
+            obj.keithley.set_output(1) % turn on keithley
             %prepare axes for plotting
             hold(ax,'on');
 % %             colors = lines(2);
 %             %plot data bin 1
-            resLaser_range = eval(obj.resLaserPower_range);
-            for i = 1 : length(resLaser_range)
-                plotH{i} = plot(obj.data.APDCounts(:,i),'parent',ax);
+%             imagesc(obj.data.probability(:,:))
+            for i = 1 : length(eval(obj.resLaserPower_range))
+                plotH{i} = plot(eval(obj.repumpLaserPower_range),obj.data.probability(:, i),'parent',ax);
             end
-            ylabel(ax,'Counts');
-            xlabel(ax,'APD Bins');
+            ylabel(ax,'Probability of initialization');
+            xlabel(ax,'Resonant Laser Power/angle');
 
 %             % Store for UpdateRun
             ax.UserData.plots = plotH;
@@ -66,12 +78,17 @@ classdef TransitionRatesCounter < Experiments.PulseSequenceSweep.PulseSequenceSw
 %             averagedData = obj.data.sumCounts;
 %             meanError = obj.data.stdCounts;
 %             end
-            
+
             %grab handles to data from axes plotted in PreRun
             for i = 1 : length(eval(obj.resLaserPower_range))
-                ax.UserData.plots{i}.YData = obj.data.APDCounts(:,i);
+                ax.UserData.plots{i}.YData = obj.data.probability(:, i);
                 drawnow limitrate;
             end        
+        end
+        
+        function PostRun(obj,~,~,~)
+            obj.keithley.delete
+            obj.arduino.delete
         end
         
         function set.resTime_us(obj,val)
---
+++ Modules/+Experiments/+PulseSequenceSweep/@InitializationBlueCounter/run.m
@@ -0,0 +1,96 @@
+function run( obj,status,managers,ax)
+% Main run method (callback for CC run button)
+obj.abort_request = false;
+status.String = 'Experiment started';
+drawnow;
+% Edit here down (save data to obj.data)
+% Tips:
+% - If using a loop, it is good practice to call:
+%     drawnow; assert(~obj.abort_request,'User aborted.');
+%     as frequently as possible
+% - try/catch/end statements useful for cleaning up
+% - You can get a figure-like object (to create subplots) by:
+%     panel = ax.Parent; delete(ax);
+%     ax(1) = subplot(1,2,1,'parent',panel);
+% - drawnow can be used to update status box message and any plots
+
+% Assert user implemented abstract properties correctly
+% assert(iscell(obj.vars)&&~isempty(obj.vars)&&min(size(obj.vars))==1,'Property "vars" should be a 1D cell array with at least one value!');
+% assert(all(cellfun(@ischar,obj.vars)),'Property "vars" should only contain strings');
+% check_prop_exists = cellfun(@(a)isprop(obj,a),obj.vars);
+% assert(all(check_prop_exists),sprintf('Properties not found in obj that are listed in "vars":\n%s',...
+%     strjoin(obj.vars(check_prop_exists),newline)));
+% assert(numel(obj.nCounterBins)==1 && isnumeric(obj.nCounterBins) && floor(obj.nCounterBins)==obj.nCounterBins,...
+%     'Property "nCounterBins" should be a single integer');
+
+obj.meta.prefs = obj.prefs2struct;
+    
+for i = 1:length(obj.vars)
+    obj.meta.vars(i).name = obj.vars{i};
+    obj.meta.vars(i).vals = obj.(obj.vars{i});
+end
+obj.meta.position = managers.Stages.position; % Stage position
+
+f = figure('visible','off','name',mfilename);
+a = axes('Parent',f);
+p = plot(NaN,'Parent',a);
+
+numresLaserPowers = length(eval(obj.resLaserPower_range));
+numrepumpLaserPower = length(eval(obj.repumpLaserPower_range));
+obj.data.APDCounts = NaN([numrepumpLaserPower, numresLaserPowers, obj.samples, obj.nCounterBins]); 
+obj.data.probability = NaN([numrepumpLaserPower, numresLaserPowers]);
+try
+    obj.PreRun(status,managers,ax);
+    % Construct APDPulseSequence once, and update apdPS.seq
+    % Not only will this be faster than constructing many times,
+    % APDPulseSequence upon deletion closes PulseBlaster connection
+    apdPS = APDPulseSequence(obj.nidaqH,obj.pbH,sequence('placeholder')); %create an instance of apdpulsesequence to avoid recreating in loop
+%     drawnow('limitrate'); assert(~obj.abort_request,'User aborted.');
+    % BuildPulseSequence must take in vars in the order listed
+    pulseSeq = obj.BuildPulseSequence;
+    n_repump_p = 1;
+    n_res_p = 1;
+    
+    for repumpPower = eval(obj.repumpLaserPower_range) % Looping over the range of repump power measurements
+        obj.keithley.set_voltage(repumpPower);
+        obj.data.repumpLaserPower(n_repump_p) = obj.keithley.get_voltage * obj.keithley.measureCurrent;
+        obj.keithley.set_voltage(0);
+        for resLaserPower = eval(obj.resLaserPower_range) % Looping over the range of resonant power measurements
+            obj.arduino.angle = resLaserPower;
+            if pulseSeq ~= false % Interpret a return of false as skip this one (leaving in NaN)
+                pulseSeq.repeat = 1; %obj.samples;
+                apdPS.seq = pulseSeq;
+                success = 0;
+                counter.start;
+                for i = 1 : obj.samples
+                    drawnow('limitrate'); assert(~obj.abort_request,'User aborted.');
+                    obj.keithley.set_voltage(repumpPower);
+                    pause(obj.repumpTime_us/1e6) % 
+                    obj.keithley.set_voltage(0);
+                    % run resonant laser pulse sequence
+                    apdPS.start(1000); % hard coded
+                    apdPS.stream(p);
+                    obj.data.APDCounts(n_repump_p, n_res_p, i, :) = reshape(p.YData,obj.nCounterBins,[])';
+                    % Whether there is counts in the initial bin
+                    if obj.data.APDCounts(n_repump_p, n_res_p, i, 1) > obj.counterDuration/2*obj.est_CountsPerSecond_cps % initialized successfully if bright for the first half of the bin
+                        success = success  + 1; 
+                    end               
+                end
+                counter.stop;
+                obj.data.probability(n_repump_p, n_res_p) = success/obj.samples;
+            end
+            obj.UpdateRun(status,managers,ax);
+            n_res_p = n_res_p + 1;
+        end
+        n_repump_p = n_repump_p + 1;
+    end
+        
+    obj.PostRun(status,managers,ax);
+    
+catch err
+end
+delete(f);
+if exist('err','var')
+    rethrow(err)
+end
+end
